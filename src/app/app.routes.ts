fcd3dd65358475588f2a1303e3f8676a77a96094
Sinouhé <sinouhes@gmail.com>
2026-04-27 19:52:32 +0200

    ci: deploying pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Hiscores } from './hiscores/hiscores';
 import { App } from './app';
 
 export const routes: Routes = [
-  { path: 'hiscores', component: Hiscores },
+  { path: 'hiscores', component: Hiscores, },
   { path: 'hiscores/:playerName', component: Hiscores },
   { path: 'hiscores/:playerName/:groupId', component: Hiscores },
   { path: '**', component: App },
