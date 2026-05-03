1d0d707a0360d20185086c635654a3317861856e
Sinouhé <sinouhes@gmail.com>
2026-05-03 21:16:22 +0200

    feat: redirect to hiscores by default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Hiscores } from './hiscores/hiscores';
 import { App } from './app';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/hiscores', pathMatch: 'full' },
   { path: 'hiscores', component: Hiscores, },
   { path: 'hiscores/:playerName', component: Hiscores },
   { path: 'hiscores/:playerName/:groupId', component: Hiscores },
